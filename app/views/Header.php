baeefe7cec7d3740cebaa67689e3c606912684fd
munt-dropjes <74962981+munt-dropjes@users.noreply.github.com>
2023-02-17 16:14:54 +0100

    navigation

--- app/views/Header.php
+++ app/views/Header.php
@@ -5,23 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/footer.css" type="text/css">
+    <link rel="stylesheet" href="css/navigation.css" type="text/css">
     <title>Hairstyling Igeo</title>
 </head>
 <body>
 <header>
     <div>
-        <nav>
-            <ul>
-                <li>
-                    <a href="/home">Home</a>
-                </li>
-                <li>
-                    <a href="/products">Maria Nila</a>
-                </li>
-                <li>
-                    <a href="/contact">Contact</a>
-                </li>
-            </ul>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/product">Maria Nila</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="/home">
+                                <img src="../public/images/logo.png" alt="Igeo Hairstyling">
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     </div>
 </header>
