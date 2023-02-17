ec48aa64e31e89b5182cc5c3edb30ba87c7555aa
munt-dropjes <74962981+munt-dropjes@users.noreply.github.com>
2023-02-17 16:47:19 +0100

    navigation shizzle

--- app/views/Header.php
+++ app/views/Header.php
@@ -4,34 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="css/footer.css" type="text/css">
+    <link rel="stylesheet" href="css/navigation.css" type="text/css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Hairstyling Igeo</title>
 </head>
 <body>
 <header>
-    <div>
+    <div class="navigation">
         <nav>
-            <div>
-                <div>
-                    <ul class="nav nav-tabs">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/home">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/product">Maria Nila</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="navbar-brand" href="/home">
-                                <img src="../public/images/logo.png" alt="Igeo Hairstyling">
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/contact">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+            <ul>
+                <li>
+                    <a class="navigation" href="/home">Home</a>
+                </li>
+                <li>
+                    <a href="/product" style="color:yellow;">Maria Nila</a>
+                </li>
+                <li>
+                    <a href="/home">
+                        <img src="../public/images/logo.png">
+                    </a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
         </nav>
     </div>
 </header>
