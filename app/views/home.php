7d59e6e84339053ad68d840f3caaecb1edbc63af
J-Blijenburg <jensblijenburg@gmail.com>
2023-02-17 15:46:54 +0100

    udpate home

--- app/views/home.php
+++ app/views/home.php
@@ -2,6 +2,7 @@
     <?php foreach($allPrices as $row){
         ?>  
             <h1> <?php echo $row->PriceID ?></h1>
+            <h1> <?php echo $row->PriceName ?></h1>
         <?php
     }?>
 </main>
