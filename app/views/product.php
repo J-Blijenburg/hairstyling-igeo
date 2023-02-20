2021cc68d1704f0263b51d079aee19e542b79e61
J-Blijenburg <jensblijenburg@gmail.com>
2023-02-20 23:57:36 +0100

    update maria nilia

--- app/views/product.php
+++ app/views/product.php
@@ -2,30 +2,43 @@
 
 
     <div class="productStructure">
-        <?php foreach ($allProducts as $row) {
-        ?>
-            <div class="productCard">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <?php $dataUri = "data:image/jpg;charset=utf;base64," . base64_encode($row->ProductImage) ?>
-                            <img src="<?php echo $dataUri; ?>" class="img-fluid rounded-start" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h4 class="card-title"><?php echo $row->ProductName ?></h4>
-                                <p class="card-text"><?php echo $row->ProductDescription ?></p>
-                                <?php $priceFormat = number_format((float)$row->ProductPrice, 2, '.', ''); ?>
-                                <p class="card-text">Prijs: € <?php echo $priceFormat ?>,-</p>
+
+        <div class="card" id="productCard">
+            <div class=cardImage>
+                <img width="100%" src="/images/test.jpeg" alt="Image is not shown">
+            </div>
+            <div class="productinformation">
+                <div class="productInformationText">
+                    A nourishing argan oil that softens, moisturizes and strengthens your hair. The argan oil quickly absorbs and makes the hair smooth while reducing frizz. True Soft Argan Oil has a soft flowery scent of vanilla, rose and jasmine.
+                </div>
+                <div class="card-body" id="contentBodyCard">
+
+                    <?php foreach ($allProducts as $row) {
+                    ?>
+                        <div class="card" id="contentSingleProduct">
+                            <div class="contentImages">
+                                <?php $dataUri = "data:image/jpg;charset=utf;base64," . base64_encode($row->ProductImage) ?>
+                                <img src="<?php echo $dataUri; ?>" class="img-fluid rounded-start" alt="Image is not shown">
+                            </div>
+                            <div class="card-body" id="contentPrice">
+                                <h5>
+                                    €
+                                    <?php
+                                    $priceFormat = number_format((float)$row->ProductPrice, 2, '.', '');
+                                    echo $priceFormat;
+                                    ?>,-
+                                </h5>
                             </div>
                         </div>
-                    </div>
+                    <?php
+
+                    } ?>
                 </div>
             </div>
 
-        <?php
-        
-        } ?>
+        </div>
+
+
     </div>
 
 
