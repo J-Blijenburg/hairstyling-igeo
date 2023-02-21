6a13af30aea0b91328463964d1d820ae45a8f19c
munt-dropjes <74962981+munt-dropjes@users.noreply.github.com>
2023-02-21 23:59:04 +0100

    added todo
    final stretch

--- app/views/contact.php
+++ app/views/contact.php
@@ -19,7 +19,7 @@
             <span class="contactPara">igeo@hotmail.nl</span>
         </div>
         <p style="width:100%;text-align:center;margin:0;">
-            Afspraken maken via Whatsapp
+            Afspraken maken via <span style="text-decoration:underline;">Whatsapp</span>
         </p>
     </div>
     <div class="contactInformation">
@@ -30,10 +30,6 @@
             <div style="text-align: center;">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2435.92710960958!2d4.6159790157507645!3d52.37174167978648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5ef602fa33c17%3A0xd279f61ee8c04463!2sHairstyling%20Igeo!5e0!3m2!1snl!2snl!4v1676844967621!5m2!1snl!2snl" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
-            <p>
-                Van Oosten de Bruijnstraat 209<br>
-                2014 VR, Haarlem
-            </p>
         </div>
         <div class="contactTimeSlots">
             <h2 class="contactTimeTableTitle">
