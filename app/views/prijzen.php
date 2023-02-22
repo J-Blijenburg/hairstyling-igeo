018c874599fc4d69896fafa1b706abfb5f582845
J-Blijenburg <jensblijenburg@gmail.com>
2023-02-23 00:41:44 +0100

    update prijzen telefoon

--- app/views/prijzen.php
+++ app/views/prijzen.php
@@ -14,7 +14,7 @@
                         <tr>
                             <td><?php echo $row->PriceName ?></td>
                             <td>&#8364;<?php $priceFormat = number_format((float)$row->PricePrice, 2, ',', '');
-                                    echo $priceFormat; ?></td>
+                                        echo $priceFormat; ?></td>
                         </tr>
                     <?php
                     } ?>
@@ -35,7 +35,7 @@
                         <tr>
                             <td><?php echo $row->PriceName ?></td>
                             <td>&#8364;<?php $priceFormat = number_format((float)$row->PricePrice, 2, ',', '');
-                                    echo $priceFormat; ?></td>
+                                        echo $priceFormat; ?></td>
                         </tr>
                     <?php
                     } ?>
@@ -56,7 +56,72 @@
                         <tr>
                             <td><?php echo $row->PriceName ?></td>
                             <td>&#8364;<?php $priceFormat = number_format((float)$row->PricePrice, 2, ',', '');
-                                    echo $priceFormat; ?></td>
+                                        echo $priceFormat; ?></td>
+                        </tr>
+                    <?php
+                    } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="hamburgerprijzenStructure">
+        <div class="prijzenCard">
+            <table class="table table-striped" id="prijzenCardTable">
+                <thead>
+                    <tr>
+                        <th scope="col">Heren</th>
+                        <th scope="col">Prijs</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($pricesHeren as $row) {
+                    ?>
+                        <tr>
+                            <td><?php echo $row->PriceName ?></td>
+                            <td>&#8364;<?php $priceFormat = number_format((float)$row->PricePrice, 2, ',', '');
+                                        echo $priceFormat; ?></td>
+                        </tr>
+                    <?php
+                    } ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="prijzenCard">
+            <table class="table table-striped" id="prijzenCardTable">
+                <thead>
+                    <tr>
+                        <th scope="col">Dames</th>
+                        <th scope="col">Prijs</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($pricesDames as $row) {
+                    ?>
+                        <tr>
+                            <td><?php echo $row->PriceName ?></td>
+                            <td>&#8364;<?php $priceFormat = number_format((float)$row->PricePrice, 2, ',', '');
+                                        echo $priceFormat; ?></td>
+                        </tr>
+                    <?php
+                    } ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="prijzenCard">
+            <table class="table table-striped" id="prijzenCardTable">
+                <thead>
+                    <tr>
+                        <th scope="col">Kinderen</th>
+                        <th scope="col">Prijs</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($pricesKinderen as $row) {
+                    ?>
+                        <tr>
+                            <td><?php echo $row->PriceName ?></td>
+                            <td>&#8364;<?php $priceFormat = number_format((float)$row->PricePrice, 2, ',', '');
+                                        echo $priceFormat; ?></td>
                         </tr>
                     <?php
                     } ?>
