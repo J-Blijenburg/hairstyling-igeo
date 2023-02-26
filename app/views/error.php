5e9d7e136f0d64a85c1e252fd10a2ec322909b55
munt-dropjes <74962981+munt-dropjes@users.noreply.github.com>
2023-02-26 22:42:14 +0100

    errors

--- app/views/error.php
+++ app/views/error.php
@@ -1,1 +1,18 @@
-Paniek   ERROR 
+<div class="container-fluid">
+    <div style="width: 100%;text-align:center;">
+        <h1>#404</h1>
+    </div>
+    </div>
+    <div class="row">
+        <div class="col">
+
+        </div>
+        <div class="col">
+            <h3>Dit is een pagina die nog niet bestaat. Het kan zijn dat je een typfout hebt gemaakt in de url.</h3>
+        </div>
+        <div class="col">
+            <img src="/images/logo.png">
+        </div>
+        <div class="col"></div>
+    </div>
+</div>
