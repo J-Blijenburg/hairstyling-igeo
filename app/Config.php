3f9648fe7fbccfd8e6d84b2ebffde7ef0d90fda5
J-Blijenburg <jensblijenburg@gmail.com>
2023-02-27 12:09:03 +0100

    mobile middelste nummer dr uit

--- app/Config.php
+++ app/Config.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "id20367896_igeodatabaseuser";
-$password = "ddjb2]s(y-kbzV>|";
-$database = "id20367896_igeodb";
+$servername = "mysql";
+$username = "root";
+$password = "secret123";
+$database = "igeo_db";
 ?>
